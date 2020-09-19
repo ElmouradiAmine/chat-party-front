a17a51e41c0ff2c1b0caf62f5aa01ec9d8390f15
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-09-19 19:02:48 +0200

    Header completed

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from "react";
 
+import Switch from "react-switch";
+
+import Colors from "./index.css";
 import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-
-        </header>
-    );
-
-}
+  console.log(Colors.colorSwitch);
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img
+          src="/images/linux-logo.png"
+          alt="logo linux chat party"
+          className="header-content__logo logo"
+        />
+        <h1 className="header-content__logo-text logo-text">
+          Chat <span className="logo-text--party">Party</span>
+        </h1>
+        {/* TODO: FIX the switch */}
+        <Switch
+          className="header-content__switch"
+          onColor={Colors.colorSwitch}
+          checked={true}
+          onChange={() => {}}
+          checkedIcon={false}
+        ></Switch>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
